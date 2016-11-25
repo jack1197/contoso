9dc984d8aa6fba071c8b7a03d9d238154afc2528
Jack Wilkie <im-not-tiger@hotmail.com>
2016-11-26 12:10:06 +1300

    Made files for facebook auth

---
+++ contoso/MyFacebook/FacebookAuth.cs
@@ -0,0 +1,13 @@
+﻿using Facebook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace contoso.MyFacebook
+{
+    public class FacebookAuth
+    {
+
+    }
+}
